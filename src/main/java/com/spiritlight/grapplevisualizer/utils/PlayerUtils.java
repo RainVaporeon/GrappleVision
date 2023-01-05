c5e765a4ac4e92895c8efb81c6b85b15beaa7061
Ori <38721999+OriSpirit@users.noreply.github.com>
2023-01-05 14:29:59 +0800

    Wynncraft compatibility for sake of weapon skins and other stuffs, thanks by the way!

--- src/main/java/com/spiritlight/grapplevisualizer/utils/PlayerUtils.java
+++ src/main/java/com/spiritlight/grapplevisualizer/utils/PlayerUtils.java
@@ -3,18 +3,35 @@ package com.spiritlight.grapplevisualizer.utils;
 import com.spiritlight.grapplevisualizer.Main;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBow;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.NBTTagString;
 import net.minecraft.util.math.RayTraceResult;
 
 public class PlayerUtils {
     public static boolean canGrapple() {
         final EntityPlayer player = Main.mc.player;
         if(player == null) return false;
-        if(!(player.inventory.getCurrentItem().getItem() instanceof ItemBow)) return false;
+        if(holdingBow()) return false;
         RayTraceResult result = player.rayTrace(Main.config.getMaxReachDistance(), 10.0f);
         return result != null && result.typeOfHit != RayTraceResult.Type.MISS;
     }
 
     public static boolean holdingBow() {
-        return Main.mc.player.inventory.getCurrentItem().getItem() instanceof ItemBow;
+        return isWynncraftBow(Main.mc.player.inventory.getCurrentItem());
+    }
+
+    public static boolean isWynncraftBow(ItemStack stack) {
+        if(stack.getTagCompound() == null) return false;
+        NBTTagCompound compound = stack.getTagCompound();
+        NBTTagList list = compound.getCompoundTag("display").getTagList("Lore", 8);
+        for(NBTBase element : list) {
+            NBTTagString string = (NBTTagString) element;
+            if(string == null) continue;
+            if(string.getString().contains("Archer/Hunter")) return true;
+        }
+        return false;
     }
 }
